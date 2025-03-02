fc3716fdba8c0e497ebd9be176002af205926dd7
Sıla Taşkın <152621799+silataskin@users.noreply.github.com>
2025-03-02 12:04:03 +0300

    ucgen cizme

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UcgenCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int derinlik = 0; 
+            Console.WriteLine("Üçgen çizmek için derinlik sayısı giriniz:");
+            derinlik = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= derinlik; i++)
+            {
+                Console.WriteLine(new string(' ',derinlik - i)+ new string('*',2*i-1));
+            }
+
+        }
+    }
+}
